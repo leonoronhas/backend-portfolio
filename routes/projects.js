31caae85ad488405dc390a451f70046ac0980679
leonoronhas <leonoronsantos@gmail.com>
2021-05-01 00:16:55 -0600

    feat: add, get all, and get a project routes

---
+++ routes/projects.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { handleUploadMiddleware } = require("../uploadSetup");
+
+const projectController = require("../controllers/projectController");
+
+// GET -> /project/all
+router.get("/all", projectController.getAllProjects);
+
+// GET -> /project/id
+router.get("/:id", projectController.getProject);
+
+// POST => /project/add
+router.post(
+  "/add",
+  handleUploadMiddleware.single("project_picture"),
+  projectController.addProject
+);
+
+module.exports = router;
