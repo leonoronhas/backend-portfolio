c1d7fa2616898592529cfc8965e75fa020957a50
leonoronhas <leonoronsantos@gmail.com>
2021-05-01 18:57:32 -0600

    feat: first iteration of routes

---
+++ routes/experience.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const experienceController = require("../controllers/experienceController");
+
+// // GET -> /experience/all
+// router.get("/all", experienceController.getAllExperiences);
+
+
+// // POST => /experience/add
+// router.post(
+//   "/add",
+//   experienceController.addExperience
+// );
+
+// // PUT => /experience/update
+// router.put(
+//   "/update/:id",
+//   experienceController.updateExperience
+// );
+
+// // DELETE => /experience/delete/:id
+// router.delete(
+//   "/delete/:id",
+//   experienceController.deleteExperience
+// );
+
+module.exports = router;
